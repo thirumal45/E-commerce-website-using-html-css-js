fd89cc16a9dc9bfc0baa11ccffc020c62d7e0992
thirumal45 <165898719+thirumal45@users.noreply.github.com>
2024-09-09 21:59:27 +0530

    Delete New folder directory

--- New folder/script.js
+++
@@ -1,8 +0,0 @@
-const bar = document.getElementById('bar');
-const nav = document.getElementById('title-link')
-
-if(bar) {
-    bar.addEventListener('click', () =>{
-        nav.classList.add('active')
-    })
-}
